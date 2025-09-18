88fc948f04f85750711b9cb7be58d5c62b1f48c8
Steve Foster <stevefosteruk@gmail.com>
2025-09-18 10:41:40 +0100

    chore: add tailwindcss

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,7 @@ function App() {
 
   return (
     <>
-      <h1>{id}</h1>
+      <h1 className="text-blue-600">{id}</h1>
       <div className="card">
         <button onClick={() => setId(generateId({separator:"-", charset:"alphanumeric"}))}>
             Generate ID
